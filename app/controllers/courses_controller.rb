87c98dea8a317197c992e4255c1b382320e08012
rachelwadsworth-smith <rachel.wadsworthsmith@gmail.com>
2020-11-19 12:25:20 -0700

    added views for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,18 @@
 class CoursesController < ApplicationController
   def index
+    @courses =Courses.all
   end
 
   def show
+    @course =Course.find(params[:id])
   end
 
   def new
+    @course = Course.new
   end
+
+  def create
+  @course = Course.create(name: params[:course][:name])
+  redirect_to @course
+end
 end
